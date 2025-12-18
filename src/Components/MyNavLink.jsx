ae4bcada32e57b175a186ef8a844ca5e6a399199
Md Abdullah <155mdabdullah@gmail.com>
2025-12-18 12:45:34 +0600

    Add Navbar component with main navigation links

--- src/Components/MyNavLink.jsx
+++ src/Components/MyNavLink.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router";
 
-const MyNavLink = () => {
+const MyNavLink = ({ to, className, children }) => {
   return (
-    <div>
-      
-    </div>
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        isActive
+          ? "fJost text-primary text-[16px] hover:text-primary hover:bg-transparent font-semibold py-2"
+          : `${className} text-white text-[16px] fJost hover:text-primary hover:bg-transparent font-semibold py-2 duration-300`
+      }
+    >
+      {children}
+    </NavLink>
   );
 };
 
