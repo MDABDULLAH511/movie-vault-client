0fe392c742fb694c9a9775936815f03369d01fd5
Md Abdullah <155mdabdullah@gmail.com>
2025-12-22 01:11:20 +0600

    Add Top Rated, Recently Added, and Genre sections

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import AboutMovieVault from "./AboutMovieVault";
+import GenreListWithImage from "./GenreListWithImage";
 import HeroCarousel from "./HeroCarousel";
+import RecentlyAddedMovies from "./RecentlyAddedMovies";
+import TopRatedMovies from "./TopRatedMovies";
 
 const Home = () => {
   return (
     <div>
       <HeroCarousel />
       <AboutMovieVault />
+      <TopRatedMovies />
+      <RecentlyAddedMovies />
+      <GenreListWithImage />
     </div>
   );
 };
