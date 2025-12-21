f9e183a0a7f5567c8fb18d6c9a7e3c0e8ff1d954
Md Abdullah <155mdabdullah@gmail.com>
2025-12-21 22:54:30 +0600

    Implement About Platform section and dynamic Statistics section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,11 @@
-
+import AboutMovieVault from "./AboutMovieVault";
 import HeroCarousel from "./HeroCarousel";
 
 const Home = () => {
   return (
     <div>
       <HeroCarousel />
-    
+      <AboutMovieVault />
     </div>
   );
 };
